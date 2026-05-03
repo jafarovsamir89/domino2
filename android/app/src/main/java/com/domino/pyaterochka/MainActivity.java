ac45d043ebc87c15a4e7e78b0f9c6d9735ee20a9
DELL <jafarovsamir@gmail.com>
2026-05-04 02:59:23 +0400

    Force mixed content for Android webview

--- android/app/src/main/java/com/domino/pyaterochka/MainActivity.java
+++ android/app/src/main/java/com/domino/pyaterochka/MainActivity.java
@@ -1,5 +1,14 @@
 package com.domino.pyaterochka;
 
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void load() {
+        super.load();
+        if (getBridge() != null && getBridge().getWebView() != null) {
+            getBridge().getWebView().getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
